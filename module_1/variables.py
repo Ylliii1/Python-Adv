8f246ecb268f411009ae1645d69fa5c6ef6b12a3
Ylliii1 <yllbytyqi180@gmail.com>
2025-06-02 17:49:59 +0200

    mod-1

--- module_1/variables.py
+++ module_1/variables.py
@@ -1,1 +1,30 @@
 print("Hello World!!")
+
+temprature = 13
+name = "Enes"
+
+print(temprature)
+print(type(temprature))
+
+print(name)
+print(type(name))
+
+
+x = 10
+y = 20
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x*y)
+
+#update values
+age = 30
+
+age = age + 1
+
+name = "Ylli"
+surename = "Bytyqi"
+
+print
