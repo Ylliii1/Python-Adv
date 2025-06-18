fe2870ef458aa898c984c8b2edc82d10ef8baa37
Ylliii1 <yllbytyqi180@gmail.com>
2025-06-18 17:46:41 +0200

    mod-1

---
+++ module_5/function.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello World")
+
+greet()
+
+def greet_person(name):
+    print("Hello,", name)
+
+greet_person("Amar")
+
+def sum(x,y):
+    z = x+y
+    return z,
+
+print()
